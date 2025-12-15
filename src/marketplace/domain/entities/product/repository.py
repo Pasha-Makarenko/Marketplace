8e12951f2817114b414d6da37b4fc3c95c62d106
Pasha-Makarenko <pashamakarenko2007@gmail.com>
2025-12-15 12:19:57 +0200

    feat(products): add product entity

---
+++ src/marketplace/domain/entities/product/repository.py
@@ -0,0 +1,27 @@
+from abc import abstractmethod
+from typing import Protocol
+
+from marketplace.domain.entities.identity import Identity
+from marketplace.domain.entities.product.product import Product
+
+
+class ProductRepository(Protocol):
+    @abstractmethod
+    async def by_identity(self, product_id: Identity) -> Product | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def list_by_seller(self, seller_id: Identity) -> list[Product]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def list_by_category(self, category_id: Identity) -> list[Product]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def add(self, product: Product) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def remove(self, product: Product) -> None:
+        raise NotImplementedError
